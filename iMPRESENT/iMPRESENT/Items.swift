6c66c93702d8df3089d07e758d5e48eddebfd0cb
Luiza <annaluza.wolf@duke.edu>
2020-06-26 21:20:14 -0400

    working yay

--- iMPRESENT/iMPRESENT/Items.swift
+++ iMPRESENT/iMPRESENT/Items.swift
@@ -26,11 +26,11 @@ class Items{
     var meditation = ""
     var stressLevelAfter = 0
     
-    var webroot = "http://localhost:3000"
-    //var webroot = "https://lit-wave-61686.herokuapp.com"
+    //var webroot = "http://localhost:3000"
+    var webroot = "https://lit-wave-61686.herokuapp.com"
     
-    var appKey = "password"
-    //var appKey = "C5Q#yE462dVB9s2S"
+    //var appKey = "password"
+    var appKey = "C5Q#yE462dVB9s2S"
     
 //    {
 //        for name in names{
